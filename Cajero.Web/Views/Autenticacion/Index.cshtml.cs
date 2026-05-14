ec9f9449a603877141f794658bda87f892f9a3de
Manuel Mejia <manuelmejia2d@gmail.com>
2026-05-14 12:34:09 -0600

    Feat: Avanze de vistas frontend

---
+++ Cajero.Web/Views/Autenticacion/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cajero.Web.Views.Autenticacion
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
